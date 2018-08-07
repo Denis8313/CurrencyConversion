a35a66b417c2db8cb74dc565d6b82d5c8a20b488
Denis <ka_bum@live.com>
2018-08-07 17:28:32 +0200

    Including basic logic of currency conversion - final stage

--- app/src/main/java/com/example/denis/currencyconversion/MainActivity.java
+++ app/src/main/java/com/example/denis/currencyconversion/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.denis.currencyconversion;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private double inputValue;
     private String currencyFrom;
     private String currencyTo;
@@ -40,26 +46,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                inputValue = Double.valueOf(editTextInputValue.getText().toString());
-                currencyFrom = spinnerFromCurrency.getSelectedItem().toString();
-                currencyTo = spinnerToCurrency.getSelectedItem().toString();
+                try {
+                    inputValue = Double.valueOf(editTextInputValue.getText().toString());
+                    currencyFrom = spinnerFromCurrency.getSelectedItem().toString();
+                    currencyTo = spinnerToCurrency.getSelectedItem().toString();
 
-                if(currencyFrom.equals("EUR") && currencyTo.equals("KN")){
-                    resultConversionBuying = 7.5;
-                    resultConversionSelling = 7.0;
-                    textViewToCurrencyStringB.setText(currencyTo.toString());
-                    textViewToCurrencyStringS.setText(currencyTo.toString());
-                }
-                else if(currencyFrom.equals("KN") && currencyTo.equals("EUR")){
-                    resultConversionBuying = 1/7.5;
-                    resultConversionSelling = 1/7.0;
-                    textViewToCurrencyStringB.setText(currencyTo.toString());
-                    textViewToCurrencyStringS.setText(currencyTo.toString());
-                }
+                    if (currencyFrom.equals("EUR") && currencyTo.equals("KN")) {
+                        resultConversionBuying = 7.5;
+                        resultConversionSelling = 7.0;
+                        textViewToCurrencyStringB.setText(currencyTo.toString());
+                        textViewToCurrencyStringS.setText(currencyTo.toString());
+                    } else if (currencyFrom.equals("KN") && currencyTo.equals("EUR")) {
+                        resultConversionBuying = 1 / 7.5;
+                        resultConversionSelling = 1 / 7.0;
+                        textViewToCurrencyStringB.setText(currencyTo.toString());
+                        textViewToCurrencyStringS.setText(currencyTo.toString());
+                    } else if (currencyFrom == currencyTo){
+                        resultConversionBuying = 1;
+                        resultConversionSelling = 1;
+                        textViewToCurrencyStringB.setText(currencyTo.toString());
+                        textViewToCurrencyStringS.setText(currencyTo.toString());
+                    }
+
 
                 textViewBuyingResult.setText(String.valueOf(inputValue*resultConversionBuying));
                 textViewSellingResult.setText(String.valueOf(inputValue*resultConversionSelling));
 
+                }catch (IllegalArgumentException exception){
+                Log.e(LOG_TAG, "Unknown number", exception);
+                Toast.makeText(getApplicationContext(), "Please enter the value", Toast.LENGTH_SHORT).show();
+            }
             }
         });
     }
