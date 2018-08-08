155a82f8013c4d644ebbc5f1d25e0dce639d5422
Denis <ka_bum@live.com>
2018-08-08 20:44:09 +0200

    Setting up android spinner in MainActivit.java

--- app/src/main/java/com/example/denis/currencyconversion/MainActivity.java
+++ app/src/main/java/com/example/denis/currencyconversion/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,8 +13,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private double inputValue;
@@ -22,25 +27,45 @@ public class MainActivity extends AppCompatActivity {
     private double resultConversionBuying;
     private double resultConversionSelling;
 
+    private EditText editTextInputValue;
+    private TextView textViewBuyingResult, textViewSellingResult, textViewToCurrencyStringB, textViewToCurrencyStringS;
+    private Spinner spinnerFromCurrency, spinnerToCurrency;
+    private Button buttonSubmitConversion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextInputValue = (EditText) findViewById(R.id.editTextEnterAmount);
+        spinnerFromCurrency = (Spinner) findViewById(R.id.spinnerFromCurrency);
+        spinnerToCurrency = (Spinner) findViewById(R.id.spinnerToCurrency);
+        textViewBuyingResult = (TextView) findViewById(R.id.textViewInputBuying);
+        textViewSellingResult = (TextView) findViewById(R.id.textViewInputSelling);
+        textViewToCurrencyStringB = (TextView) findViewById(R.id.textViewCurrencyBuying);
+        textViewToCurrencyStringS = (TextView) findViewById(R.id.textViewCurrencySelling);
+
+        buttonSubmitConversion = (Button) findViewById(R.id.buttonSubmitConversion);
+
+        List entries = new ArrayList();
+        entries.add("KN");
+        entries.add("EUR");
+        entries.add("USD");
+        entries.add("JPN");
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, entries);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinnerFromCurrency.setAdapter(spinnerAdapter);
+        spinnerToCurrency.setAdapter(spinnerAdapter);
+
+
+
         updateUi();
     }
 
     public void updateUi() {
-
-        final EditText editTextInputValue = (EditText) findViewById(R.id.editTextEnterAmount);
-        final Spinner spinnerFromCurrency = (Spinner) findViewById(R.id.spinnerFromCurrency);
-        final Spinner spinnerToCurrency = (Spinner) findViewById(R.id.spinnerToCurrency);
-        final TextView textViewBuyingResult = (TextView) findViewById(R.id.textViewInputBuying);
-        final TextView textViewSellingResult = (TextView) findViewById(R.id.textViewInputSelling);
-        final TextView textViewToCurrencyStringB = (TextView) findViewById(R.id.textViewCurrencyBuying);
-        final TextView textViewToCurrencyStringS = (TextView) findViewById(R.id.textViewCurrencySelling);
-
-        Button buttonSubmitConversion = (Button) findViewById(R.id.buttonSubmitConversion);
 
         buttonSubmitConversion.setOnClickListener(new View.OnClickListener() {
             @Override
